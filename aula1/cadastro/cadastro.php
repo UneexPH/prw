20086224cb0aaecf0db78bdd955ba8bba7836e95
uneexph <pedro_arrieiro@hotmail.com>
2022-03-30 10:56:10 -0300

    a

---
+++ aula1/cadastro/cadastro.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "<h1> Cadastro PHP </h1> ";
+   $nome = $_GET['nome'];
+   $email = $_GET['email'];
+
+   echo 'Nome: ' . $nome . '<br>';
+   echo 'E-mail: ' . $email . '<br>';
+
+
+?>
--- aula1/ex5.php
+++ aula1/ex5.php
@@ -1,6 +1,51 @@
-<?
+<?php
 
 //data 
-echo "Data Atual: ".date('d/m/Y');
+echo "Data Atual: ".date('d/m/Y'). "<br>";
+echo " Horário: ".date('H:i:s')."<br>" ;
+
+//--------------------------------------------------------------------
+
+$palavra = "  Ciência da Computação ";
+echo "A variável contém: " . strlen($palavra) . "caracteres" . "<br>";
+$palavra = trim($palavra); //comando para tirar os espaços da palavra
+echo "A variável agora contém: " . strlen($palavra) . "caracteres"."<br>";
+
+//----------------------------------------------------------------------
+
+//substituir parte das strings
+
+$data = date("m-d-Y");
+$dia = substr($data,3,2);
+$mes = substr($data,0,2);
+$ano = substr($data,6,4);
+$novadata = $dia . "/" . $mes . "/" . $ano;
+echo "<br>".$data;
+echo "<br>".$novadata."<br>";
+
+//-------------------------------------------------------------------
+
+//separar dados a partir de algum caractere
+$data = date("m-d-Y");
+$partes = explode("-", $data);
+$parte1 = $partes[0];
+$parte2 = $partes[1];
+$parte3 = $partes[2];
+$novadata = $parte2 . "/" . $parte1 . "/" . $parte3;
+echo "<br>".$data;
+echo "<br>".$novadata."<br>"."<br>";
+
+//-------------------------------------------------------------------------
+
+//Strtoupper converte todos os caracteres contidos em uma string em letras maiúsculas.
+//Strtolower converte todos os caracteres contidos em uma string em letras minúsculas.
+
+$A = "EiTa noIS!" ;
+$B = strtoupper($A);
+$C = strtolower($A);
+echo $A ."<br>";
+echo $B ."<br>";
+echo $C ."<br>";
+
 
 ?>

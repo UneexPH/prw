1a73b568f0bb772de13f08e347d4fd8b415b2b7b
pedro <pedro_arrieiro@hotmail.com>
2022-04-17 15:12:27 -0300

    a

---
+++ lista 5/ex1.php
@@ -0,0 +1,11 @@
+<?php
+//1. (Formulário e Vetor) Fazer um formulário em HTML que permita ao usuário digitar o
+//nome de um produto de informática. Um programa em PHP deverá receber o nome
+//desse produto e fazer uma busca por esse produto dentro de uma relação de produtos
+//de informática em um vetor e localizar a posição (índice) desse produto dentro do vetor.
+//O índice corresponde ao código do produto que deve ser impresso para o usuário.
+
+
+
+
+?>
---
+++ lista 5/ex2.php
@@ -0,0 +1,6 @@
+<?php
+//2.(Formulário e Vetor)Repetir o exercício anterior, mas dessa vez deve ter 2 vetores, um
+//com os nomes dos produtos e outro com os preços desses produtos, que devem ser
+//impressos após a localização do mesmo.
+
+?>

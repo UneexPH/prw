414d9d0ec033f318c6d998ccc781fa5a2ce93927
pedro <pedro_arrieiro@hotmail.com>
2022-05-11 12:12:43 -0300

    a

--- Lista6/Agenda.sql
+++ Lista6/Agenda.sql
@@ -11,10 +11,10 @@ create table agenda (
     endereco varchar(70) not null,
     bairro varchar(50) not null,
     cidade varchar(50) not null,
-    estado varchar(02) not null,
+    estado varchar(2) not null,
     telefone varchar(15) not null,
     celular varchar(15) not null,
     email varchar(70) not null,
-    dt_cadastro date not null
+    dt_cadastro date not null,
     PRIMARY KEY (id_agenda)
-)
+);
